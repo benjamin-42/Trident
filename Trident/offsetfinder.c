e3179af0ae4d333c1056a116458efe37038d7fba
EGYbkgo9449 <Xbox.ps3.psp.ps2@gmail.com>
2017-01-26 00:25:06 -0800

    Update offsetfinder.c

--- Trident/offsetfinder.c
+++ Trident/offsetfinder.c
@@ -1,17 +1,17 @@
 uint32_t find_OSSymbol_getMetaClass(void) {
-	switch (target_environment) {
-		case iPhone53_iOS932: return 0x321818;
-	}
+    switch (target_environment) {
+	case iPhone53_iOS932: return 0x321818;
+    }
 }
 
 uint32_t find_calend_gettime(void) {
-	switch (target_environment) {
-		case iPhone53_iOS932: return 0x1ee6c;
-	}
+    switch (target_environment) {
+        case iPhone53_iOS932: return 0x1ee6c;
+    }
 }
 
 uint32_t find_bufattr_cpx(void) {
-	switch (target_environment) {
-		case iPhone53_iOS932: return 0xdea48;		
-	}
+    switch (target_environment) {
+	case iPhone53_iOS932: return 0xdea48;		
+    }
 }
